6028ff99979629eb4e8db425d20e7068996cd842
かゆい <128781460+kayui-gavo@users.noreply.github.com>
2026-09-16 08:32:42 +0900

    tests: lock UI audited import routing

---
+++ tests/test_ui_import_routing.py
@@ -0,0 +1,12 @@
+import tabito_itemgen.exam_production as exam_production
+import tabito_itemgen.exam_ui as exam_ui
+import tabito_itemgen.exam_ui_entrypoint as exam_ui_entrypoint
+import tabito_itemgen.response_audit as response_audit
+
+
+def test_teacher_runtime_routes_model_responses_through_audited_import():
+    assert exam_ui.import_section_response is response_audit.import_section_response
+
+
+def test_history_restore_keeps_provenance_neutral_core_import():
+    assert exam_ui_entrypoint.core_import_section_response is exam_production.import_section_response
