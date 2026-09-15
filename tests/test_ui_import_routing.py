4cab86939c0fa57e080c79dde9c93749606f3750
かゆい <128781460+kayui-gavo@users.noreply.github.com>
2026-09-16 08:34:36 +0900

    tests: lock launcher to policy entrypoint

--- tests/test_ui_import_routing.py
+++ tests/test_ui_import_routing.py
@@ -2,6 +2,7 @@ import tabito_itemgen.exam_production as exam_production
 import tabito_itemgen.exam_ui as exam_ui
 import tabito_itemgen.exam_ui_entrypoint as exam_ui_entrypoint
 import tabito_itemgen.response_audit as response_audit
+from tabito_itemgen.ui_launcher import app_path
 
 
 def test_teacher_runtime_routes_model_responses_through_audited_import():
@@ -10,3 +11,7 @@ def test_teacher_runtime_routes_model_responses_through_audited_import():
 
 def test_history_restore_keeps_provenance_neutral_core_import():
     assert exam_ui_entrypoint.core_import_section_response is exam_production.import_section_response
+
+
+def test_launcher_uses_policy_entrypoint_not_raw_exam_ui():
+    assert app_path().name == "exam_ui_entrypoint.py"
